c72426254ab4905947c19d10473cf649b1009ddc
Adam Jahr <atomjar@Adams-MacBook-Pro.local>
2017-02-13 14:35:40 -0500

    new files

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
 require('aframe');
-require('aframe-audioanalyser-component');
-require('aframe-particle-system-component');
-
-
-
-var entity = document.querySelector('[sound]');
-
-function playSound () {
-  entity.components.sound.playSound();
-}
